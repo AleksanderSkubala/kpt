d35bd75490a560191e97527b14467a1315b7d1e0
Aleksander Skubała <35501014+AleksanderSkubala@users.noreply.github.com>
2018-08-26 22:10:36 +0200

    Add hiding elements

--- src/components/FormComponents/FormMain.vue
+++ src/components/FormComponents/FormMain.vue
@@ -14,9 +14,10 @@
                     Nazwa zbiórki:
                 </td>
                 <td>
-                    <div class="ui action input">
-                        <input placeholder="Nazwa/Temat zbiórki" type="text" v-model="nazwa"/>
-                        <div class="ui positive button" @click="halo(`${nazwa}`)">
+                    <p v-if="edit[0] === false">{{nazwa.content}}</p>
+                    <div class="ui action input" v-if="edit[0]">
+                        <input placeholder="Nazwa/Temat zbiórki" type="text" v-model="nazwa.content"/>
+                        <div class="ui positive button" @click="halo(`${nazwa.content}`, 0)">
                             <i class="check icon" style="margin:0;"></i>
                         </div>
                         <div class="ui negative button">
@@ -31,7 +32,7 @@
                 </td>
                 <td>
                     <div class="ui action input">
-                        <input placeholder="Imię i Nazwisko" type="text" :value="odpowiedzialny"/>
+                        <input placeholder="Imię i Nazwisko" type="text" v-model="odpowiedzialny"/>
                         <div class="ui positive button" @click="halo(`${odpowiedzialny}`)">
                             <i class="check icon" style="margin:0;"></i>
                         </div>
@@ -43,17 +44,11 @@
             </tr>
             <tr>
                 <td>
-                    Prowadący:
-                </td>
-                <td>Initial commit</td>
-            </tr>
-            <tr>
-                <td>
                     Termin:
                 </td>
                 <td>
                      <div class="ui action input">
-                        <input type="date" v-model="kiedy"/>
+                        <input type="date" v-model="kiedy" :placeholder="dzisiaj"/>
                         <div class="ui positive button" @click="halo(`${kiedy}`)">
                             <i class="check icon" style="margin:0;"></i>
                         </div>
@@ -61,6 +56,25 @@
                             <i class="plus icon" style="margin:0; transform: rotate(45deg);"></i>
                         </div>
                     </div>
+                </td>
+            </tr>
+            <tr style="background: none;">
+                <td colspan="2">
+                   <table class="ui striped table">
+                        <thead>
+                            <tr>
+                                <th>Prowadzący:</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Jaki Prowadzący</td>
+                            </tr>
+                            <tr>
+                                <td>Jaki Prowadzący2</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </td>
             </tr>
             <tr>
@@ -142,21 +156,34 @@ export default {
     name: "FormMain",
     data(){
         return{
-            nazwa:"",
+            dzisiaj: "",
+            nazwa: {
+                content: "",
+                edit: true,
+            },
             odpowiedzialny: "",
             kiedy: "",
+            edit: [
+                true
+            ],
         };
     },
     methods: {
-        halo(co) {
+        halo(co, ed) {
             if(co){
                 console.log(co);
+                this.edit[ed] = false;
+                console.log(this.edit[ed]);
             }
         },
         empty(co) {
-            co = null;
+
             console.log(co);
         },
+    },
+    mounted() {
+        this.dzisiaj = (new Date()).getDate();
+        console.log(this.dzisiaj);
     },
 }
 </script>

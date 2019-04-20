9902837b1d6dc1735f12cfbce6dde66c79fab239
AleksanderSkubala <olek_s@wtvk.pl>
2019-04-20 22:22:50 +0200

    Finish cutting prototyp for one page

--- public/cutting.js
+++ public/cutting.js
@@ -1,27 +1,34 @@
-NodeList.prototype.cut = function(){
-    var i = this.length;
+const outerHeight = function(el){
+    var elHeight = parseInt(window.getComputedStyle(el).getPropertyValue('height'));
+    elHeight += parseInt(window.getComputedStyle(el).getPropertyValue('margin-top'));
+    elHeight += parseInt(window.getComputedStyle(el).getPropertyValue('margin-bottom'));
+    return elHeight;
+};
+
+NodeList.prototype.cut = function(marginBottom){
     var height = 0;
     var pages = 1;
     var margin = 0;
+    var lastMargin = 0;
 
     this.forEach(el => {
-        height += el.offsetHeight;
-        pages = height/1123;
+        height += outerHeight(el);
+        pages = height/(1123-marginBottom);
+
         if(pages>1) {
             pages = Math.floor(pages);
-            margin = el.offsetHeight-(height-(pages*1123));
-            //setting the margin on the value, that element is on the sheet
-            el.style.offsetTop = el.offsetTop+margin+112;
-            //positioning the element with the border of the page with 10%(of 1123) margin
-
-            //it all will be in for loop that will repeat this for all pages
+            margin = outerHeight(el)-(height-(pages*1123)); //margin var is equal height of the piece of element that is on the sheet
+            el.style.marginTop = `${margin+lastMargin+marginBottom}px`; //positioning the element on the border of the page with marginBot
+        } else {
+            lastMargin = parseInt(window.getComputedStyle(el).getPropertyValue('margin-bottom')); //setting last(for next iteration) margin bottom as present margin botom
         }
     });
-
-
-    console.log('lenght:',i,'height:',height);
 };
 
 var elements = document.querySelectorAll('.box');
 console.log(elements);
-elements.cut();
+elements.cut(100);
+
+// USAGE:
+// NodeList.cut(margin);
+// margin [int] => margin on the bottom and top of the page

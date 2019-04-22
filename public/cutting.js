2f95559216d992e59de585096ee5bfc9880c500d
AleksanderSkubala <olek_s@wtvk.pl>
2019-04-22 22:03:43 +0200

    finish with cutting prototype

--- public/cutting.js
+++ public/cutting.js
@@ -9,9 +9,8 @@ const cutFun = function(arr, mar) {
     arr.cut(mar);
 };
 
-NodeList.prototype.cut = function(marginBottom){
-    console.log(this);
-    var parent = this[0].parentNode;
+NodeList.prototype.cut = function(marginBottom = 0){
+    var parent = this[0];
     var index = 0;
     var height = 0;
     var pages = 1;
@@ -19,10 +18,11 @@ NodeList.prototype.cut = function(marginBottom){
     var lastMargin = 0;
 
     var nodes = this;
-    var fragment = document.createElement('div');
+    var fragment = document.createDocumentFragment();
 
     for(var i = 0; i < this.length; i++) {
         var el = this[i];
+        parent = el.parentNode;
         height += outerHeight(el);
         pages = height/(1123-marginBottom);
 
@@ -30,18 +30,13 @@ NodeList.prototype.cut = function(marginBottom){
             pages = Math.floor(pages);
             margin = outerHeight(el)-(height-(pages*1123)); //margin var is equal height of the piece of element that is on the sheet
             el.style.marginTop = `${margin+lastMargin+marginBottom}px`; //positioning the element on the border of the page with marginBot
-            console.log('pages',pages);
-            console.log('margin',margin);
-            console.log('heigth',height);
 
-            //multipage functionality
-            for (var i2 = index; i2 < this.length; i2++) {
-                fragment.appendChild(this[i2]);
-                parent.appendChild(this[i2]);
-            }
-            nodes = fragment.childNodes;
-            nodes.cut(marginBottom);
-            break;
+            //multipage functionality => resetting all values
+            index = 0;
+            height = outerHeight(el);
+            pages = 1;
+            margin = 0;
+            lastMargin = 0;
         } else {
             lastMargin = parseInt(window.getComputedStyle(el).getPropertyValue('margin-bottom')); //setting last(for next iteration) margin bottom as present margin botom
             index += 1;
@@ -51,8 +46,8 @@ NodeList.prototype.cut = function(marginBottom){
 
 var elements = document.querySelectorAll('.box');
 console.log(elements);
-elements.cut(100);
+elements.cut();
 
 // USAGE:
 // NodeList.cut(margin);
-// margin [int] => margin on the bottom and top of the page
+// margin [int, 0 by default] => margin on the bottom and top of the page
